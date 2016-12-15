cd3963a3bca0a4d1f4f25e3b77d49dff6368f304
xrefor <xrefor@gmail.com>
2016-12-15 09:37:47 +0100

    fixed SRV func and added some colors.

--- digall.go
+++ digall.go
@@ -4,80 +4,89 @@ import (
         "fmt"
         "os"
         "net"
+                "github.com/fatih/color"
 )
 
 //Lookup functions
 func aRecord(query string) {
-    ipRecord, err := net.LookupIP(query)
-    fmt.Printf("\n[+] A Record\n")
+        ipRecord, err := net.LookupIP(query)
+    color.Cyan("[+] A Record\n")
     for i := 0; i < len(ipRecord); i++ {
         fmt.Printf("IP : %s \n", ipRecord[i])
     }
-        
         if err != nil {
                 //panic(err)
         }
+        return
 }
 
 func cnameRecord(query string) {
-    cnameRecord, err := net.LookupCNAME(query)
-    fmt.Printf("\n[+] CNAME Records\n")
-    fmt.Println(cnameRecord, err)
-    if err != nil {
-        //panic(err)
-    }
+        cnameRecord, err := net.LookupCNAME(query)
+        color.Cyan("\n[+] CNAME Records\n")
+        fmt.Println(cnameRecord, err)
+        if err != nil {
+                //panic(err)
+        }
+        return
 }
 
 func mxRecord(query string) {
         mxRecord, err := net.LookupMX(query)
-        fmt.Printf("\n[+] MX Records\n")
+        color.Cyan("\n[+] MX Records\n")
         for i := 0; i < len(mxRecord); i++ {
-            fmt.Printf("Host : %s Priority : %d \n", mxRecord[i].Host, mxRecord[i].Pref)
+                fmt.Printf("Host : %s Priority : %d \n", mxRecord[i].Host, mxRecord[i].Pref)
         }
 
         if err != nil {
-           //panic(err)
+                //panic(err)
         }
+        return
 }
 
 func txtRecord(query string) {
         txtRecord, err := net.LookupTXT(query)
-        fmt.Printf("\n[+] TXT Record(s)\n")
+        color.Cyan("\n[+] TXT Record(s)\n")
         for i := 0; i < len(txtRecord); i++ {
-            fmt.Printf("#%d : %s \n", i, txtRecord[i])
+                fmt.Printf("#%d : %s \n", i, txtRecord[i])
         }
-        
         if err != nil {
                 //panic(err)
         }
+        return
 }
 
 func srvRecord(query string) {
-        fmt.Printf("\n[+] SRV Record(s)\n")
+        color.Cyan("\n[+] SRV Record(s)\n")
         services := [...]string{"sipfederationtls", "autodiscover", "tls", "_sip", "xmpp-server", "VLMCS"}
         for _, service := range services {
-            cname , addrs, err := net.LookupSRV(service, "tcp", query)
-            
-            if err != nil {
-                    //panic(err)
-            }
-            for _, record := range addrs {
-                fmt.Printf("Target: %s:%d\n",cname, record.Target, record.Port)
-            }
+                cname, addrs, err := net.LookupSRV(service, "tcp", query)
+                for i := 0; i < len(addrs); i++ {
+                        fmt.Printf("addrs[%d].Target : %s \n", i, addrs[i].Target)
+                        fmt.Printf("addrs[%d].Port : %d \n", i, addrs[i].Port)
+                        fmt.Printf("addrs[%d].Priority : %d \n", i, addrs[i].Priority)
+                        fmt.Printf("addrs[%d].Weight : %d \n", i, addrs[i].Weight)
+                        color.Cyan("cname : %s \n", cname)
+                }
+                if err != nil {
+                        //panic(err)
+                }
+                //for _, record := range addrs {
+                //fmt.Printf("Target: %s:%d\n",cname, record.Target, record.Port,)
+                //}
         }
-
+        return
 }
 
 func nsRecord(query string) {
         nsRecord, err := net.LookupNS(query)
-        fmt.Printf("\n[+] NS Records\n")
+        color.Cyan("\n[+] NS Records\n")
         for i := 0; i < len(nsRecord); i++ {
-            fmt.Printf("NS : %s \n", nsRecord[i].Host)
+                fmt.Printf("NS : %s \n", nsRecord[i].Host)
         }
-        
         if err != nil {
                 //panic(err)
         }
+        return
 }
 
 func main() {
@@ -88,9 +97,9 @@ func main() {
                 // variable for A record subdomain
                 www := fmt.Sprintf("www.")
                 //Run query functions with info
-                fmt.Printf("\n[ digall: Starting DNS queries ]\n")
+                color.Yellow("\n[ digall: Starting DNS queries ]\n")
                 aRecord(query)
-                fmt.Printf("\n[!] with www.")
+                color.Magenta("\n[!] with www. :")
                 aRecord(www+query)
                 cnameRecord(www+query)
                 mxRecord(query)
@@ -98,10 +107,10 @@ func main() {
                 srvRecord(query)
                 fmt.Printf("\n")
                 nsRecord(query)
-                fmt.Printf("\n[!] Done\n")
+                color.Green("\n[!] Done\n")
 
                 } else {
-                fmt.Print("ERROR: Not a valid argument\n")
-                fmt.Println("Usage: ./digall <domain>\n[!] QUITTING")
+                color.Red("ERROR: Not a valid argument\n")
+                color.Yellow("Usage: ./digall <domain>\n[!] QUITTING")
         }
 }
