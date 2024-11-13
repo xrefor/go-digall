0b713112272fbe10709d1bec7932123d68baf79b
xrefor <xrefor@gmail.com>
2024-11-13 13:50:57 +0100

    Updated go-digall with loading indicator  and color enhancements

--- digall.go
+++ digall.go
@@ -1,129 +1,173 @@
+
 package main
 
 import (
-	"fmt"
-	"github.com/fatih/color"
-	"golang.org/x/net/idna"
-	"net"
-	"net/url"
-	"os"
+    "fmt"
+    "github.com/briandowns/spinner"
+    "github.com/fatih/color"
+    "net"
+    "os"
+    "time"
 )
 
+// DNSRecordFetcher holds methods to fetch various DNS record types.
+type DNSRecordFetcher struct {
+    domain string
+    s      *spinner.Spinner
+}
+
+// NewDNSRecordFetcher initializes a DNSRecordFetcher for the specified domain with a spinner.
+func NewDNSRecordFetcher(domain string) *DNSRecordFetcher {
+    s := spinner.New(spinner.CharSets[11], 100*time.Millisecond) // Use a clean spinner character set
+    s.Prefix = "Loading... "
+    return &DNSRecordFetcher{domain: domain, s: s}
+}
+
+// FetchARecords fetches A records (IPv4 addresses) for the domain.
+func (d *DNSRecordFetcher) FetchARecords() {
+    d.s.Start()
+    if records, err := net.LookupIP(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgGreen).Println("A Records:")
+        for _, ip := range records {
+            if ip.To4() != nil {
+                fmt.Println(ip)
+            }
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchAAAARecords fetches AAAA records (IPv6 addresses) for the domain.
+func (d *DNSRecordFetcher) FetchAAAARecords() {
+    d.s.Start()
+    if records, err := net.LookupIP(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgCyan).Println("AAAA Records:")
+        for _, ip := range records {
+            if ip.To16() != nil && ip.To4() == nil {
+                fmt.Println(ip)
+            }
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchMXRecords fetches MX records for the domain.
+func (d *DNSRecordFetcher) FetchMXRecords() {
+    d.s.Start()
+    if records, err := net.LookupMX(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgYellow).Println("MX Records:")
+        for _, mx := range records {
+            fmt.Printf("%s %d\n", mx.Host, mx.Pref)
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchNSRecords fetches NS records for the domain.
+func (d *DNSRecordFetcher) FetchNSRecords() {
+    d.s.Start()
+    if records, err := net.LookupNS(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgBlue).Println("NS Records:")
+        for _, ns := range records {
+            fmt.Println(ns.Host)
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchTXTRecords fetches TXT records for the domain.
+func (d *DNSRecordFetcher) FetchTXTRecords() {
+    d.s.Start()
+    if records, err := net.LookupTXT(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgMagenta).Println("TXT Records:")
+        for _, txt := range records {
+            fmt.Println(txt)
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchCAARecords fetches CAA records for the domain.
+func (d *DNSRecordFetcher) FetchCAARecords() {
+    d.s.Start()
+    if records, err := net.LookupTXT(d.domain); err == nil && len(records) > 0 {
+        d.s.Stop()
+        color.New(color.FgHiYellow).Println("CAA Records:")
+        for _, caa := range records {
+            fmt.Println(caa)
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchCNAME fetches the CNAME record for the domain.
+func (d *DNSRecordFetcher) FetchCNAME() {
+    d.s.Start()
+    if cname, err := net.LookupCNAME(d.domain); err == nil && cname != d.domain {
+        d.s.Stop()
+        color.New(color.FgHiGreen).Println("CNAME Record:", cname)
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchPTRRecords fetches PTR records (reverse DNS lookup) for the domain.
+func (d *DNSRecordFetcher) FetchPTRRecords() {
+    d.s.Start()
+    ips, err := net.LookupIP(d.domain)
+    if err == nil && len(ips) > 0 {
+        d.s.Stop()
+        color.New(color.FgRed).Println("PTR Records:")
+        for _, ip := range ips {
+            if names, err := net.LookupAddr(ip.String()); err == nil && len(names) > 0 {
+                for _, name := range names {
+                    fmt.Println(name)
+                }
+            }
+        }
+        fmt.Println()
+    } else {
+        d.s.Stop()
+    }
+}
+
+// FetchAllRecords fetches all DNS records and displays them if present.
+func (d *DNSRecordFetcher) FetchAllRecords() {
+    fmt.Printf("Fetching DNS records for domain: %s\n\n", d.domain)
+    d.FetchARecords()
+    d.FetchAAAARecords()
+    d.FetchMXRecords()
+    d.FetchNSRecords()
+    d.FetchTXTRecords()
+    d.FetchCAARecords()
+    d.FetchCNAME()
+    d.FetchPTRRecords()
+}
+
 func main() {
-	if len(os.Args) > 1 {
-		//Query argument parsed and encoded to ASCII
-		//in case of foreign letters(e.g æøå) usage: (digall <hostname>)
-		u, err := url.Parse("http://" + os.Args[1])
-		if err != nil {
-			fmt.Println(err)
-		}
-		query, err := idna.ToASCII(u.Host)
-		if err != nil {
-			fmt.Println(err)
-		}
-		// variable for A record subdomain
-		www := "www."
-		//Used to check protocoll of SRV Record (See below)
-		proto := [3]string{"tcp", "tls", "udp"}
-
-		//Run query functions and print info
-		//----------------------------------
-		//A Records
-		color.Yellow("[+] Starting DNS queries\n")
-		color.Cyan("[+] A Records\n")
-		color.Magenta("[*] " + query)
-		aRecord(query)
-		color.Magenta("[*] www." + query)
-		aRecord(www + query)
-		//CNAME Records
-		color.Cyan("\n[+] CNAME Record(www." + query + ")")
-		cnameRecord(www + query)
-		//MX Records
-		color.Cyan("\n[+] MX Records\n")
-		mxRecord(query)
-		//TXT Records
-		color.Cyan("\n[+] TXT Record(s)\n")
-		txtRecord(query)
-		//SRV Records using slice proto to check protocoll tcp, udp and tls
-		color.Cyan("\n[+] SRV Record(s)\n")
-		srvRecord(query, proto[0])
-		srvRecord(query, proto[1])
-		srvRecord(query, proto[2])
-		fmt.Printf("\n")
-		//NS Records
-		nsRecord(query)
-		color.Green("[+] Done\n")
-
-	} else {
-		color.Red("[-] ERROR: Not a valid argument\n")
-		color.Yellow("[!] Usage: ./digall <hostname>\n[!] QUITTING")
-	}
-}
-
-//Lookup functions
-func aRecord(query string) {
-	ipRecord, err := net.LookupIP(query)
-	for i := 0; i < len(ipRecord); i++ {
-		fmt.Printf("IP : %s \n", ipRecord[i])
-	}
-	if err != nil {
-		//panic(err)
-	}
-}
-
-func cnameRecord(query string) {
-	cnameRecord, err := net.LookupCNAME(query)
-	fmt.Println(cnameRecord)
-	if err != nil {
-		//panic(err)
-	}
-}
-
-func mxRecord(query string) {
-	mxRecord, err := net.LookupMX(query)
-	for i := 0; i < len(mxRecord); i++ {
-		fmt.Printf("Host : %s Priority : %d \n", mxRecord[i].Host, mxRecord[i].Pref)
-	}
-	if err != nil {
-		//panic(err)
-	}
-}
-
-func txtRecord(query string) {
-	txtRecord, err := net.LookupTXT(query)
-	for i := 0; i < len(txtRecord); i++ {
-		fmt.Printf("#%d : %s \n", i, txtRecord[i])
-	}
-	if err != nil {
-		//panic(err)
-	}
-}
-
-func srvRecord(query, proto string) {
-	services := [...]string{"sipfederationtls", "autodiscover", "sip", "tls", "tcp", "xmpp-server", "VLMCS", "pbx", "h323ls",
-		"ts3", "stun"}
-	for _, service := range services {
-		cname, addrs, err := net.LookupSRV(service, proto, query)
-		for i := 0; i < len(addrs); i++ {
-			fmt.Printf("addrs[%d].Target : %s \n", i, addrs[i].Target)
-			fmt.Printf("addrs[%d].Port : %d \n", i, addrs[i].Port)
-			fmt.Printf("addrs[%d].Priority : %d \n", i, addrs[i].Priority)
-			fmt.Printf("addrs[%d].Weight : %d \n", i, addrs[i].Weight)
-			color.Cyan("cname : %s \n", cname)
-		}
-		if err != nil {
-			//panic(err)
-		}
-	}
-}
-
-func nsRecord(query string) {
-	nsRecord, err := net.LookupNS(query)
-	color.Cyan("[+] NS Records\n")
-	for i := 0; i < len(nsRecord); i++ {
-		fmt.Printf("NS : %s \n", nsRecord[i].Host)
-	}
-	if err != nil {
-		//panic(err)
-	}
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: go-digall <domain>")
+        os.Exit(1)
+    }
+    domain := os.Args[1]
+    fetcher := NewDNSRecordFetcher(domain)
+    fetcher.FetchAllRecords()
 }
---
+++ go.mod
@@ -0,0 +1,12 @@
+module go-digall
+
+go 1.23.2
+
+require (
+	github.com/briandowns/spinner v1.23.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
+)
